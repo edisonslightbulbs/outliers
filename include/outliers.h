8064f4864a7bdf5ff18da3627c8c7a8d469ea016
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-26 10:54:08 +0100

    adding function doc

--- include/outliers.h
+++ include/outliers.h
@@ -5,7 +5,28 @@
 #include <vector>
 
 namespace outliers {
+/**
+ * remove
+ *   Finds and removes outliers from a vector of points.
+ *
+ * @param points
+ *   Vector of 3D points.
+ *
+ * @retval
+ *    Vector of 3D points without outliers.
+ */
 std::vector<Point> remove(std::vector<Point>& points);
+
+/**
+ * remove
+ *   Finds and removes outliers from a vector of floats.
+ *
+ * @param
+ *   Vector of float values.
+ *
+ * @retval
+ *    Vector of floats without outliers.
+ */
 std::vector<float> remove(std::vector<float>& points);
 }
-#endif /* LDA_H */
+#endif /* LDA_H  */
